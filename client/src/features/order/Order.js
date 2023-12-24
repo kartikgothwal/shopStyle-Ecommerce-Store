11bfa0810de6795b49e6093ad3834d60cef663c7
Kartik Gothwal <kartikgothwal28@gmail.com>
2023-12-24 19:37:01 +0530

    added order summary page

--- client/src/features/order/Order.js
+++ client/src/features/order/Order.js
@@ -359,6 +359,7 @@ const Order = () => {
                   <button
                     className="mt-4 w-full bg-neutral-950 text-neutral-400 border border-neutral-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group flex gap-4 justify-center items-center"
                     onClick={(e) => handlePlaceOrderClick(e)}
+                    disabled={payment.pending}
                   >
                     <span className="bg-neutral-400 shadow-neutral-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
                     Place Order{" "}
